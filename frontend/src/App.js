c2d2af0144acddc0ddf9cbf31f3787a5a820b49b
NatthaponSoisangwan <nsoisang@macalester.edu>
2019-10-18 16:38:59 -0500

    merges

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,26 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 // import { makeStyles } from "@material-ui/core/styles";
 import AppNav from "./Components/TopNav";
 import ReviewContents from "./Components/ReviewContents";
-import ReviewForm from "./Components/ReviewForm";
 import BottomNav from "./Components/BottomNav";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Review from "./Components/Review";
-
+import Review from "./Components/AddReviewModal";
+import Menu from './Components/Menu'
 
 class App extends Component {
+
     render() {
         return (
             <React.Fragment>
-                <CssBaseline />
-                <AppNav />
-                <div className="contentContainer">
-                    <Router>
-                        <Switch>
-                            <Route path='/' exact component={ReviewContents} />
-                            <Route path='/reviewform' component={ReviewForm} />
-                        </Switch>
-                    </Router>
-                </div>
-                <Review/>
-                <BottomNav />
+                <Router>
+                    <CssBaseline />
+                    <AppNav />
+                    <Switch>
+                        <Route path='/review' exact component={ReviewContents} />
+                        <Route path='/menu' component={Menu} />
+                    </Switch>
+                    <Review />
+                    <BottomNav />
+                </Router>
             </React.Fragment>
         );
     }
