6ef7254f5901af7f6b043781ce3fc4c6d9cc3880
Yifan Wu <ywu@macalester.edu>
2019-10-19 11:49:49 -0500

    Added imagefield

--- backend/review/models.py
+++ backend/review/models.py
@@ -19,7 +19,7 @@ class Review(models.Model):
     title = models.CharField("Food Title", max_length=120)
     stars = models.CharField("Star Rating", max_length=1, choices=STARS)
     description = models.TextField("Description")
-# img here
+    image = models.ImageField(upload_to='review_images')
     name = models.CharField("Name", max_length=255, blank=True)
     createdAt = models.DateTimeField("Created At", auto_now_add=True)
 
