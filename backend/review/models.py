bb823b5c91f5530f1deddde677c1fe43792e2a08
Aron <aronsmithdonovan@gmail.com>
2019-10-19 11:09:54 -0500

    Edited models.py to fix STARS values and add names for all columns

--- backend/review/models.py
+++ backend/review/models.py
@@ -3,23 +3,26 @@ from django.db import models
 
 # Create your models here.
 
-# add this
+# Review:
+# stores Review object as db file review_review.sqlite3
 class Review(models.Model):
+    # sets permitted values for stars
     STARS = (
-    ('1', 'One'),
-    ('2', 'Two'),
-    ('3', 'Three'),
-    ('4', 'Four'),
-    ('5', 'Five'),
+        1,
+        2,
+        3,
+        4,
+        5
     )
-    title = models.CharField(max_length=120)
-    # stars = models.CharField(max_length=1, choices=STARS)
-    stars = models.CharField(max_length=120)
 
-    description = models.TextField()
+    # creates columns and data types for review_review.sqlite3
+    title = models.CharField("Food Name", max_length=120)
+    stars = models.CharField("Star Rating", max_length=1, choices=STARS)
+    description = models.TextField("Description")
 # img here
-    name = models.CharField("Name", max_length=255)
-    createdAt = models.DateTimeField("Created At", auto_now_add=True)
+    name = models.CharField("Poster Name", max_length=255)
+    createdAt = models.DateTimeField("Creation DateTime", auto_now_add=True)
 
+    # stars = models.CharField("Star Rating", max_length=120)
     def _str_(self):
         return self.title
