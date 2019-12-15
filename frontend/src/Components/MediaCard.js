a184875c64bff97da859f1160a1c69a143579beb
Vichym <vmeas@macalester.edu>
2019-12-15 15:35:23 -0600

    Update Media Card

--- frontend/src/Components/MediaCard.js
+++ frontend/src/Components/MediaCard.js
@@ -4,10 +4,8 @@ import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-
 import React from "react";
 import Rating from "@material-ui/lab/Rating";
-// import Modal from "./components/Modal";
 
 const useStyles = makeStyles({
     card: {
@@ -17,31 +15,31 @@ const useStyles = makeStyles({
     media: {
         height: 140
     }
+
 });
-
-
 
 export default function MediaCard(props) {
     const classes = useStyles();
-    const { title, stars, description, image, name } = props;
+    const { menu_name, rating, description, created_date, image } = props;
 
     return (
         <Card className={classes.card}>
             <CardActionArea>
                 <CardMedia
                     className={classes.media}
-                    image={image} // TODO : Still need to fix the api link in the backend. There is no returned image. 
-                    title={name}
+                    image={image} // TODO : Still need to fix the api link in the backend. There is no returned image.
                 />
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        {title}
+                    <Typography gutterBottom variant="h5" component="h3">
+                        {menu_name}
                     </Typography>
-                    <Rating readOnly value= {parseInt(stars)} />
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {name}
+                        <Rating readOnly value={rating} align="center" />
+                    <Typography variant="body2" color="textSecondary">
+                        {new Date(created_date).getUTCMonth() + " / " +
+                            new Date(created_date).getUTCDate() + " / " +
+                            new Date(created_date).getUTCFullYear()}
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
+                    <Typography variant="h6" color="textSecondary">
                         {description}
                     </Typography>
                 </CardContent>
