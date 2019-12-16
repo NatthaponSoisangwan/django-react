1611b7e21f43253f66d2dcc74c3c20548ac6ec6e
Vichym <vmeas@macalester.edu>
2019-12-16 16:36:32 -0600

    Add Menu Page to Admin Page

--- backend/review/admin.py
+++ backend/review/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Review  # add this
-
+from .models import Review, Menu  # add this
 
 class ReviewAdmin(admin.ModelAdmin):  # add this
     list_display = ("menu_name",
@@ -10,6 +9,14 @@ class ReviewAdmin(admin.ModelAdmin):  # add this
         "created_date",
         "image",)
 
+class MenuAdmin(admin.ModelAdmin):  # add this
+    list_display = ("menu_name",
+    "meal_time",
+    "meal_vegan", 
+    "meal_vegetarian",
+    "meal_gluten_free",
+    "id",)
 
 # Register your models here.
 admin.site.register(Review, ReviewAdmin)  # add this
+admin.site.register(Menu, MenuAdmin)  # add this
